602132f8a346c028dd554c2ae65ddb28580f118c
BloodJuice <falcon.with.hat@gmail.com>
2023-08-26 21:11:18 +0300

    First point is working!

--- Veronika/Veronica.py
+++ Veronika/Veronica.py
@@ -1,22 +1,42 @@
 import scipy.stats as sps
 import numpy as np
 
+def writeFile(Distr, N, name):
+    with open(name, 'w') as file:
+        file.write(name + '(создано не ISW) N=' + str(N) + ' ' + '\n')
+        file.write(str('0 ' + ' ' + str(N) + '\n'))
+        for i in range(N):
+            file.write(str(Distr[i][0]) + '\n')
 
-def normalDestribution(xi, params):
+
+def normalDestributionFi(xi, params):
     return sps.norm(loc=params[0], scale=params[1]).cdf(xi)
 
 def Dminus_n(xi, params, n):
     Dlocal = []
     for i in range(n):
-        Dlocal.append(normalDestribution(xi, params) - (i - 1) / n)
+        Dlocal.append(normalDestributionFi(xi[i], params) - (i - 1.) / n)
     return max(Dlocal)
 
+def Dplus_n(xi, params, n):
+    Dlocal = []
+    for i in range(n):
+        Dlocal.append(i / n - normalDestributionFi(xi[i], params))
+    return max(Dlocal)
+
+def Vn(params, n, N): # N - число испытаний, n - объём выборки
+    Vnlocal = np.zeros((N, 1))
+    for i in range(N):
+        xi = sorted(np.random.normal(loc=0., scale=1., size=n))
+        Vnlocal[i][0] = Dplus_n(xi, params, n) + Dminus_n(xi, params, n)
+        print(i)
+    return Vnlocal
 
 def main():
-    volume, params = 100, [0., 1.]  # params = [loc= something value, scale= something value]
-    xi = np.random.normal(loc=0., scale=1., size=volume)
-    Dminus_n(xi, params, n=volume)
+    volume, params, N = 10, [0., 1.], 1000  # params = [loc= something value, scale= something value]
+    kuperDestributionModified = Vn(params, n=volume, N=N) * (np.sqrt(volume) + 0.155 + 0.24 / np.sqrt(volume))
 
+    writeFile(kuperDestributionModified, N, "Купер.dat")
 
-if __name__ == "main":
-    main()
+# if __name__ == "main":
+main()

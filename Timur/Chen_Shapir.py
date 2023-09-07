5acc40ce3eda06dd0390d3b64819107826611e2b
BloodJuice <falcon.with.hat@gmail.com>
2023-09-07 12:36:18 +0300

    It works

--- Timur/Chen_Shapir.py
+++ Timur/Chen_Shapir.py
@@ -32,55 +32,42 @@ def xMiddle(data, n):
     return x_middle
 def sCount(f, n, xmiddle):
     x = 0.
-    for i in range(int(n)):
+    for i in range(n):
         x += pow(f[i] - xmiddle, 2)
     x = math.sqrt((1. / (n - 1.)) * x)
     return x
 
-def F(i, n, F0):
-    masX, masF, saver = [], [], []
+def F(i, n, x0, F0):
+    saver = []
+
+    valueForCompare = (i - 3. / 8.) / (n + 1. / 4.)
+
     for j in range(len(F0)):
-        if j % 2 == 0:
-            masX.append(F0[j])
-        elif j % 2 != 0:
-            masF.append(F0[j])
-
-    coef = (i - 3. / 8.) / (n + 1. / 4.)
-    valueForCompare = abs(coef - 0.5)
-
-    for j in range(len(masF)):
-        saver.append(abs(valueForCompare - masF[j]))
+        saver.append(abs(valueForCompare - F0[j]))
     saverMin = min(saver)
-    for j in range(len(masF)):
+    for j in range(len(F0)):
         if saverMin == saver[j]:
-            res = masX[j]
+            res = x0[j]
     return res
-def sum_Coefficient(xi, n, F0):
+def sum_Coefficient(xi, n, x0, F0):
     sum = 0.
     for i in range(n):
         if (i + 1) > (n - 1):
-           sum += (xi[n - 1] - xi[i]) / (pow(F(i + 2, n, F0), -1) - pow(F(i + 1, n, F0), -1))
+           sum += (xi[n - 1] - xi[i]) / (pow(F(i + 2, n, x0, F0), 1) - (pow(F(i + 1, n, x0, F0), 1)))
         else:
-            y = xi[i + 1] - xi[i]
-            a = pow(F(i + 2, n, F0), -1)
-            b = pow(F(i + 1, n, F0), -1)
-            z = a - b
-            if a == b:
-                sum += 0.
-            else:
-                sum += y / z
+            sum += (xi[i + 1] - xi[i]) / (pow(F(i + 2, n, x0, F0), 1) - (pow(F(i + 1, n, x0, F0), 1)))
     return sum
 def QN(n, N):
     masX, masF0 = readFile()
     saver = []
     for i in range(N):
         xi = sorted(np.random.normal(0., 1., n))
-        qn = 1. / (sCount(xi, n, xMiddle(xi, n)) * (n - 1.)) * sum_Coefficient(xi, n, masF0)
-        saver.append(np.sqrt(n) * (1 - qn))
+        qn = 1. / (sCount(xi, n, xMiddle(xi, n)) * (n - 1.)) * sum_Coefficient(xi, n, masX, masF0)
+        saver.append(np.sqrt(n) * (1. - qn))
         print(i)
     return saver
 def main():
-    n, N = 10, 1000
+    n, N = 100, 16600
     Distr = (QN(n, N))
     write_In_File_Distr(Distr, N, "Чен-ШапирQnx", n)
 

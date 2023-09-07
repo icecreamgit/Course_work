372a5532ce967f588153a226703c62a9a64ab5d0
BloodJuice <falcon.with.hat@gmail.com>
2023-09-07 11:09:02 +0300

    Get norm Func

--- Timur/Chen_Shapir.py
+++ Timur/Chen_Shapir.py
@@ -8,18 +8,16 @@ from PyPDF2 import PdfReader
 
 
 def readFile():
-    reader = PdfReader("laplasetable2.pdf")
-    a = 1
-    number_of_pages = len(reader.pages)
-    page = reader.pages[0]
-    texts = (page.extract_text()).split()
-    text = (re.findall(r'\d*\,\d+|\d+', str(texts)))
-    for i in range(len(text)):
-        text[i] = text[i].replace(",", ".")
-    text = [float(i) for i in text]
-    return text
+    masX = []
+    masF0 = []
+    with open("F0.txt", 'r') as file:
+        for i in file:
+            lineSplit = i.split()
+            masX.append(float(lineSplit[0]))
+            masF0.append(float(lineSplit[1]))
+    return masX, masF0
 
-def write_In_File(Distr, N, name, n):
+def write_In_File_Distr(Distr, N, name, n):
     with open(name + '_' + str(n) + '_' + str(N) + '.dat', 'w') as file:
         file.write(name + '(создано не ISW) N=' + str(N) + '\t n = ' + str(n) +  ' ' + '\n')
         file.write(str('0 ' + ' ' + str(N) + '\n'))
@@ -41,7 +39,6 @@ def sCount(f, n, xmiddle):
 
 def F(i, n, F0):
     masX, masF, saver = [], [], []
-    eps, res = 0.01, 0.
     for j in range(len(F0)):
         if j % 2 == 0:
             masX.append(F0[j])
@@ -49,37 +46,42 @@ def F(i, n, F0):
             masF.append(F0[j])
 
     coef = (i - 3. / 8.) / (n + 1. / 4.)
-    valueForCompare = round(abs(coef - 0.5), 4)
+    valueForCompare = abs(coef - 0.5)
+
     for j in range(len(masF)):
-        a = round(abs(valueForCompare - masF[j]), 4)
-        if a <= eps:
+        saver.append(abs(valueForCompare - masF[j]))
+    saverMin = min(saver)
+    for j in range(len(masF)):
+        if saverMin == saver[j]:
             res = masX[j]
-            break
     return res
 def sum_Coefficient(xi, n, F0):
-    sum, eps = 0., 0.0001
+    sum = 0.
     for i in range(n):
         if (i + 1) > (n - 1):
            sum += (xi[n - 1] - xi[i]) / (pow(F(i + 2, n, F0), -1) - pow(F(i + 1, n, F0), -1))
         else:
             y = xi[i + 1] - xi[i]
-            a = pow(F(i + 2, n, F0) + eps, -1)
-            b = pow(F(i + 1, n, F0) + eps, -1)
+            a = pow(F(i + 2, n, F0), -1)
+            b = pow(F(i + 1, n, F0), -1)
             z = a - b
-            sum += y / z
+            if a == b:
+                sum += 0.
+            else:
+                sum += y / z
     return sum
 def QN(n, N):
-    valueF0 = readFile()
+    masX, masF0 = readFile()
     saver = []
-    # for i in range(N):
-    xi = sorted(np.random.normal(0., 1., n))
-    qn = 1. / (sCount(xi, n, xMiddle(xi, n)) * (n - 1.)) * sum_Coefficient(xi, n, valueF0)
-    saver.append(np.sqrt(n) * (1 - qn))
-    print(saver)
+    for i in range(N):
+        xi = sorted(np.random.normal(0., 1., n))
+        qn = 1. / (sCount(xi, n, xMiddle(xi, n)) * (n - 1.)) * sum_Coefficient(xi, n, masF0)
+        saver.append(np.sqrt(n) * (1 - qn))
+        print(i)
     return saver
 def main():
     n, N = 10, 1000
     Distr = (QN(n, N))
-    # write_In_File(Distr, N, "Чен-Шапир", n)
+    write_In_File_Distr(Distr, N, "Чен-ШапирQnx", n)
 
 main()
